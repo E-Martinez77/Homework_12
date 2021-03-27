85b9d5178bb2475495891622bce1b4a37857d451
E-Martinez77 <emartizzle2185@me.com>
2021-03-27 09:22:51 -0600

    Fixed syntax errors

--- seed.sql
+++ seed.sql
@@ -29,8 +29,8 @@ INSERT INTO department (name)
 VALUES ("Sales"), ("Engineering"), ("Accounting"), ("Legal"), ("Custodial");
 
 INSERT INTO role (title, salary, department_id)
-VALUES ("Sales Lead", 45,000, 1), ("Sales Rep", 30,000, 2), ("Lead Engineer", 50,000, 3), ("Software Engineer", 50,000, 4), ("Accountant", 45,000, 5), ("Legal Analyst", 75,000, 6), ("Custodian", 25,000, 7)
+VALUES ("Sales Lead", 45000, 1), ("Sales Rep", 30000, 2), ("Lead Engineer", 50000, 3), ("Software Engineer", 50000, 4), ("Accountant", 45000, 5), ("Legal Analyst", 75000, 6), ("Custodian", 25000, 7);
 
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ("Marvin", "Harris", 1, 1), ("Sheila", "Rivera", 3, 3), ("Usman", "Gupta", 2, 1), ("Robert", "Miller", 4, 1), ("Eloy", "Martinez", 7, 1)
+VALUES ("Marvin", "Harris", 1, 1), ("Sheila", "Rivera", 3, 3), ("Usman", "Gupta", 2, 1), ("Robert", "Miller", 4, 1), ("Eloy", "Martinez", 7, 1);
 
