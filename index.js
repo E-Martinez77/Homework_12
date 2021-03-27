f5e1b43b337f48c9f22dcd6df2133ead451658fe
E-Martinez77 <emartizzle2185@me.com>
2021-03-26 20:03:13 -0600

    working inquirer model

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ const cTable = require("console.table");
 const inquirer = require("inquirer");
 
 function init() {
+  console.log("Let's get started");
   function startApp() {
-    console.log("Let's get started");
     inquirer
       .prompt([
         {
@@ -37,38 +37,46 @@ function init() {
             addEmployee();
             break;
           case "Remove Employee":
-            console.log("Fire someone?");
             fireEmployee();
             break;
           case "Update Employee Role":
-            console.log("Promote someone?");
             promoteEmployee();
             break;
           case "Update Employee Manager":
-            console.log("New Manager?");
             updateManager();
             break;
         }
       });
   }
   startApp();
-}
-
-function showOrg() {
-  console.log("Who works here?");
-  init();
-}
-
-function showDept() {
-  console.log("Who works where?");
-}
-
-function showMngmnt() {
-  console.log("Who works for who?");
-}
-
-function addEmployee() {
-  console.log("Hire someone?");
+  function showOrg() {
+    console.log("Who works here?");
+    startApp();
+  }
+  function showDept() {
+    console.log("Who works where?");
+    startApp();
+  }
+  function showMngmnt() {
+    console.log("Who works for who?");
+    startApp();
+  }
+  function addEmployee() {
+    console.log("Hire someone?");
+    startApp();
+  }
+  function fireEmployee() {
+    console.log("Fire someone?");
+    startApp();
+  }
+  function promoteEmployee() {
+    console.log("Promote someone?");
+    startApp();
+  }
+  function updateManager() {
+    console.log("New Manager?");
+    startApp();
+  }
 }
 
 init();
