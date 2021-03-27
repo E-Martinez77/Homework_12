8ff4872072528e8ebd14197cc560ef2a0b1aee4d
E-Martinez77 <emartizzle2185@me.com>
2021-03-27 09:12:43 -0600

    Updated Seeds

--- seed.sql
+++ seed.sql
@@ -1,19 +1,17 @@
-DROP DATABASE IF EXISTS employee_trackerDB;
-
 CREATE DATABASE employee_trackerDB;
 
 USE employee_trackerDB;
 
 CREATE TABLE department (
   id INT NOT NULL AUTO_INCREMENT,
-  name VARCHAR(30) NULL,
+  name VARCHAR(30) NOT NULL,
   PRIMARY KEY (id)
 );
 
 CREATE TABLE role (
   id INT NOT NULL AUTO_INCREMENT,
-  title VARCHAR(30) NULL,
-  salary DECIMAL(10,2) NULL,
+  title VARCHAR(30) NOT NULL,
+  salary DECIMAL(10,2) NOT NULL,
   department_id INT,
   PRIMARY KEY (id)
 );
@@ -27,11 +25,12 @@ CREATE TABLE employee (
   PRIMARY KEY (id)
 );
 
-INSERT INTO products (flavor, price, quantity)
-VALUES ("vanilla", 2.50, 100);
+INSERT INTO department (name)
+VALUES ("Sales"), ("Engineering"), ("Accounting"), ("Legal"), ("Custodial");
 
-INSERT INTO products (flavor, price, quantity)
-VALUES ("chocolate", 3.10, 120);
+INSERT INTO role (title, salary, department_id)
+VALUES ("Sales Lead", 45,000, 1), ("Sales Rep", 30,000, 2), ("Lead Engineer", 50,000, 3), ("Software Engineer", 50,000, 4), ("Accountant", 45,000, 5), ("Legal Analyst", 75,000, 6), ("Custodian", 25,000, 7)
 
-INSERT INTO products (flavor, price, quantity)
-VALUES ("strawberry", 3.25, 75);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Marvin", "Harris", 1, 1), ("Sheila", "Rivera", 3, 3), ("Usman", "Gupta", 2, 1), ("Robert", "Miller", 4, 1), ("Eloy", "Martinez", 7, 1)
+
