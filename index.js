77de293c88eeddca91a197c1adadf1af57dc09c9
E-Martinez77 <emartizzle2185@me.com>
2021-03-26 19:33:53 -0600

    Set up first inquirer question

--- index.js
+++ index.js
@@ -1,5 +1,25 @@
 const mysql = require("mysql");
-
 const cTable = require("console.table");
-
 const inquirer = require("inquirer");
+
+function startApp() {
+  console.log("Let's get started");
+  inquirer.prompt([
+    {
+      type: "list",
+      name: "watDo",
+      message: "What would you like to do?",
+      choices: [
+        "View All Employees",
+        "View All Employees By Department",
+        "View all Employees By Manager",
+        "Add Employee",
+        "Remove Employee",
+        "Update Employee Role",
+        "Update Employee Manager",
+      ],
+    },
+  ]);
+}
+
+startApp();
