c0861dfc642972642db1f82c58fbbe33c815d378
E-Martinez77 <emartizzle2185@me.com>
2021-03-31 16:57:47 -0600

    added foreign keys

--- seed.sql
+++ seed.sql
@@ -1,3 +1,5 @@
+DROP DATABASE IF EXISTS employee_trackerDB;
+
 CREATE DATABASE employee_trackerDB;
 
 USE employee_trackerDB;
@@ -13,6 +15,8 @@ CREATE TABLE role (
   title VARCHAR(30) NOT NULL,
   salary DECIMAL(10,2) NOT NULL,
   department_id INT,
+  INDEX dep_ind (department_id),
+  CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE CASCADE,
   PRIMARY KEY (id)
 );
 
@@ -21,7 +25,11 @@ CREATE TABLE employee (
   first_name VARCHAR(30),
   last_name VARCHAR(30),
   role_id INT,
+  INDEX role_ind (role_id),
+  CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE CASCADE,
   manager_id INT,
+  INDEX man_ind (manager_id),
+  CONSTRAINT fk_manager FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE SET NULL,
   PRIMARY KEY (id)
 );
 
@@ -29,8 +37,8 @@ INSERT INTO department (name)
 VALUES ("Sales"), ("Engineering"), ("Accounting"), ("Legal"), ("Custodial");
 
 INSERT INTO role (title, salary, department_id)
-VALUES ("Sales Lead", 45000, 1), ("Sales Rep", 30000, 2), ("Lead Engineer", 50000, 3), ("Software Engineer", 50000, 4), ("Accountant", 45000, 5), ("Legal Analyst", 75000, 6), ("Custodian", 25000, 7);
+VALUES ("Sales Lead", 45000, 1), ("Sales Rep", 30000, 1), ("Lead Engineer", 50000, 2), ("Software Engineer", 50000, 2), ("Accountant", 45000, 3), ("Legal Analyst", 75000, 4), ("Custodian", 25000, 5);
 
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ("Marvin", "Harris", 1, 1), ("Sheila", "Rivera", 3, 3), ("Usman", "Gupta", 2, 1), ("Robert", "Miller", 4, 1), ("Eloy", "Martinez", 7, 1);
+VALUES ("Marvin", "Harris", 1, NULL),("Usman", "Gupta", 2, 1), ("Sheila", "Rivera", 3, 2), ("Robert", "Miller", 4, 1), ("Eloy", "Martinez", 5, 1);
 
